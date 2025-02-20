22335c2b7509e0f666f95cd09701964cf71a9a78
Fadhil Taufiqurrachman <fadhiltaufiqurrachman08@gmail.com>
2025-02-20 09:28:12 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa08.java
@@ -0,0 +1,36 @@
+public class Mahasiswa08 {
+    String nim, nama, kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Kelas : " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipk > 4.0 || ipk < 0) {
+            System.out.println("IPK Tidak Valid. Harus Antara 0.0 - 4.0");
+            ipk = 0;
+        } else {
+            ipk = ipkBaru;
+        }
+    }
+
+    String menilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
