4a161088332894bc50deae039b0ae4667a1a3fe6
Fadhil Taufiqurrachman <fadhiltaufiqurrachman08@gmail.com>
2025-02-27 13:26:11 +0700

    Modifikasi Percobaan 3

--- Jobsheet3/MataKuliah08.java
+++ Jobsheet3/MataKuliah08.java
@@ -1,7 +1,8 @@
 package Jobsheet3;
+import java.util.Scanner;
 
 public class MataKuliah08 {
-    public String kode, nama;
+    public String kode, nama, dummy;
     public int sks, jumlahJam;
 
     public MataKuliah08(String kode, String nama, int sks, int jumlahJam) {
@@ -9,5 +10,28 @@ public class MataKuliah08 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    void tambahData() {
+        Scanner input = new Scanner(System.in);
+            System.out.print("Kode          : ");
+            kode = input.nextLine();
+            System.out.print("Nama          : ");
+            nama = input.nextLine();
+            System.out.print("SKS           : ");
+            dummy = input.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam    : ");
+            dummy = input.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("-------------------------------------");
+    }
+
+    void cetakInfo() {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("SKS           : " + sks);
+        System.out.println("Jumlah Jam    : " + jumlahJam);
+        System.out.println("-------------------------------------");
     }
 }
--- Jobsheet3/MataKuliahDemo08.java
+++ Jobsheet3/MataKuliahDemo08.java
@@ -4,34 +4,23 @@ import java.util.Scanner;
 public class MataKuliahDemo08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        MataKuliah08[] arrayOfMataKuliah = new MataKuliah08[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        String kode, nama;
+        int sks, jumlahJam, k;
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan Jumlah Mata Kuliah : ");
+        k = input.nextInt();
+        input.nextLine();
+        MataKuliah08[] arrayOfMataKuliah = new MataKuliah08[k];
+
+        for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Masukkan Data Mata Kuliah Ke-" + (i + 1));
-            System.out.print("Kode          : ");
-            kode = input.nextLine();
-            System.out.print("Nama          : ");
-            nama = input.nextLine();
-            System.out.print("SKS           : ");
-            dummy = input.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam    : ");
-            dummy = input.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("-------------------------------------");
-
-            arrayOfMataKuliah[i] = new MataKuliah08(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah[i] = new MataKuliah08(kode = null, nama = null, sks = 0, jumlahJam = 0);
+            arrayOfMataKuliah[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayOfMataKuliah.length; i++) {
             System.out.println("Data Mata Kuliah Ke-" + (i + 1));
-            System.out.println("Kode          : " + arrayOfMataKuliah[i].kode);
-            System.out.println("Nama          : " + arrayOfMataKuliah[i].nama);
-            System.out.println("SKS           : " + arrayOfMataKuliah[i].sks);
-            System.out.println("Jumlah Jam    : " + arrayOfMataKuliah[i].jumlahJam);
-            System.out.println("-------------------------------------");
+            arrayOfMataKuliah[i].cetakInfo();
         }
         input.close();
     }
