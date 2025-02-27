fe0c3e4cd67b5bc02dab29a09e5705c59bca7b58
Fadhil Taufiqurrachman <fadhiltaufiqurrachman08@gmail.com>
2025-02-27 11:22:12 +0700

    Percobaan 2

--- Jobsheet3/Mahasiswa08.java
+++ Jobsheet3/Mahasiswa08.java
@@ -1,5 +1,13 @@
 package Jobsheet3;
 public class Mahasiswa08 {
     public String nim, nama, kelas;
-    public float ipk; 
+    public float ipk;
+    
+    void cetakInfo() {
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("-----------------------------------");
+    }
 }
